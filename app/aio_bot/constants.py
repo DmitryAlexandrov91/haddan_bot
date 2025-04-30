c27e226c02cb5122234d8287443984214be91771
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2025-04-30 20:46:37 +0300

    successful transfer to python 3.13 version! aiogram bot started

---
+++ app/aio_bot/constants.py
@@ -0,0 +1,5 @@
+"""Константы aiogram бота."""
+import os
+
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', None)
---
+++ app/aio_bot/main.py
@@ -0,0 +1,21 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+
+from constants import TELEGRAM_BOT_TOKEN
+
+
+if TELEGRAM_BOT_TOKEN:
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+dp = Dispatcher()
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
